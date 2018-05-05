efeb19889dc00f83b520776b311506f6b92fe35e
laurenchun <35704045+laurenchun@users.noreply.github.com>
2018-05-05 16:20:26 -0400

    Add files via upload

--- assignments/csstypeface/java.js
+++ assignments/csstypeface/java.js
@@ -1,27 +1,36 @@
-$('.letters').each(function() {
+$(function() {
+  var $input = $('#text-input');
+  var $destination = $('#text-destination');
+  var space = getHTMLForCharacter(' ');
 
-  var $letterWrapper = $(this);
-  var words = $letterWrapper.html().split(' ');
-  var wordArray = [];
+  function getHTMLForCharacter(character) {
+    var characterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
+    var characterHTML = '<div class="letter" data-character="' + character + '">' + characterDefinition + '</div>';
 
-  for (var i = 0; i < words.length; i++) {
-    var word = words[i];
-    var letters = word.split('');
-    var letterElements = letters.map(getHTMLForCharacter);
-    wordArray.push(
-    	'<div class="word">' +
-    	letterElements.join('') +
-      '</div>'
-    );
+    if ( characterDefinition != undefined ) {
+      return characterHTML;
+    }
   }
 
-  console.log(wordArray.join(getHTMLForCharacter(' ')));
+  function writeText() {
+    var wordArray = [];
+    var words = $input.val().split(' ');
 
-  $letterWrapper.html(wordArray.join(getHTMLForCharacter(' ')));
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var letters = word.split('');
+      var letterElements = letters.map(getHTMLForCharacter);
+      wordArray.push(
+        '<div class="word">' +
+        letterElements.join('') +
+        '</div>'
+      );
+    }
+
+    $destination.html( wordArray.join(space) );
+  }
+
+  $input.on('keyup', function() {
+    writeText();
+  });
 });
-
-function getHTMLForCharacter(character) {
-    	var characterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
-    	var characterHTML = '<div class="letter" data-character="' + character + '">' + characterDefinition + '</div>';
-      return characterHTML;
-}
